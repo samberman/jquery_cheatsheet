42a983d592a3fe33d77b202fa6f7e3ea83d1cb07
Sam Berman <samuelaberman@gmail.com>
2016-04-17 23:35:57 -0400

    updating jquery cheetsheet

---
+++ js/cheatsheet.js
@@ -0,0 +1,35 @@
+// document.ready allows you to assign
+// events to page elements, so that when
+// a user interacts with a page in a given
+// way, something happens on-screen
+$(document).ready(function() {
+    $("div").fadeOut(1000);
+});
+// in order for anything to happen, the
+// programmer must target the page element
+// by entering the id, class, or tag name
+// in their code. In the above example,
+// I am telling the div to fade out.
+// Referring to a css element allows the programmer
+// to associate an action to that element.
+
+// dotNotation
+$(document).ready(function() {
+    $("div").fadeOut(1000);
+// dotNotation allows us to assign actions
+// to page elements. In the above example above, I am telling
+// all the divs in the page to fade out.
+
+// Linking jquery to html
+<head>
+        <title></title>
+        <script src="js/example.js"</script>
+        
+    </head>
+
+// In order for the web browser to read the
+// jquery code, we must attach a separatte .js file via the <script> tag.
+alert("Hello")
+$(document).ready(function(){
+
+}
